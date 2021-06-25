0a80bdd374738a250da3396f825d2c0f9d432197
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2021-06-24 22:04:15 -0500

    fixing some error handling and adding secret word array

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,13 @@ var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
 var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialCharactersArray = [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ',', '^', '_', '`', '{', '}', '|', '~', '"'];
 var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-var superArray = []; // this is gonna contain whatever the user chooses (yes not no) into one giant array that concatinates from the other arrays
-var generateBtn = document.querySelector('#generate');
+var secretWordArray = ['wizard'];
+// this is gonna contain whatever the user chooses (yes not no) into one giant array that concatinates from the other arrays
+var superArray = [];
 
+var generateBtn = document.querySelector('#generate');
 // function that grabs our main function, generatePassword(), and spits out the value of the function return to that div with id="passwod" in our index.html
+
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector('#password');
@@ -23,28 +26,31 @@ generateBtn.addEventListener('click', writePassword);
 // 3. then concat the true boolean responses into our superArray 
 // 4. randomly grab characters from our customized superArray and customized length of characters specified by the user
 // 5. finally, print that randomized superArray as text to the div with id="password" (the box just above the button that calls the function generatePassword)
+
 function generatePassword() {
     // welcome pop up - confirm boolean 
-    var welcomeMsg = confirm('welcome to password generator wizard! you will be selecting at least 1 character type, ready to begin?');
+    var welcomeMsg = confirm('welcome to password generator wizard! ready to begin?');
     console.log(welcomeMsg);
     if (welcomeMsg === true) {
         console.log('yes they love my wizard and want to start');
     } else {
         console.log('your loss');
         alert('your loss');
-        window.close;
+        Window.close();
     }
     // length pop up -  prompt for user to enter a number
     var lengthMsg = parseInt(prompt('please enter the length of characters you would like for your password, it must be between 9 and 128'));
     console.log(lengthMsg);
-    if ((lengthMsg >= 9 && lengthMsg <= 128) && (!isNaN(lengthMsg))) {
+    if (lengthMsg >= 9 && lengthMsg <= 128) {
         console.log('success! this number is approved by dev wizard jessamyn');
-    } else if (isNaN(lengthMsg)) {
-        console.log('wrong, try again');
-        alert('this needs to be a number, not a wingding, also it needs to be between 9 and 128 please submit again or I\'ll close your window');
-        parseInt(prompt('lets try this again, how many characters would you like? enter a number between 9 and 128 characters please'));
+        // } else if (isNaN(lengthMsg) || '') {
+        //     console.log('wrong, try again');
+        //     alert('this needs to be a number, not a wingding, also it needs to be between 9 and 128 please submit again or I\'ll close your window');
+        //     parseInt(prompt('lets try this again, how many characters would you like? enter a number between 9 and 128 characters please'));
     } else {
-        console.log('what are you doing, this wizard can\'t wizard itself...')
+        console.log('what are you doing, this wizard can\'t wizard itself...');
+        alert('this needs to be a number, not a wingding, also it needs to be between 9 and 128 please submit again or I\'ll close your window');
+        Window.close();
     }
 
     // lowercase pop up - confirm boolean
@@ -56,7 +62,6 @@ function generatePassword() {
     } else {
         console.log('too bad, lowercase is cool');
     }
-
     // logging the superarray to see whatsup
     console.log('this is superArray after the lowercase: ', superArray);
 
@@ -69,7 +74,6 @@ function generatePassword() {
     } else {
         console.log('too bad, UPPERCASE is cool');
     }
-
     // logging the superarray to see whatsup
     console.log('this is superArray after the uppercase: ', superArray);
 
@@ -82,7 +86,6 @@ function generatePassword() {
     } else {
         console.log('too bad, numbers are cool');
     }
-
     // logging the superarray to see whatsup
     console.log('this is superArray after the numbers: ', superArray);
 
@@ -95,23 +98,34 @@ function generatePassword() {
     } else {
         console.log('too bad, special !@#$%^&*() are really cool');
     }
-
-    // if all booleans return false aka user says no/cancel to every option do this:
-    if ((lowercaseMsg && uppercaseMsg && numberMsg && specialCharMsg) === false) {
-        alert('you gotta choose something ya ding dong! let\'s try this again...')
-
-    }
-
     // logging the superarray to see whatsup
     console.log('this is superArray after the special characters !@#$%^&*()?!@#: ', superArray);
 
+    // secret word pop up - confirm boolean
+    var secretMsg = confirm('do you want to include THE SECRET WORD in your password?');
+    console.log(secretMsg);
+    if (secretMsg === true) {
+        console.log('cool give em the secret word...');
+        superArray = superArray.concat(secretWordArray);
+    } else {
+        console.log('too bad, the secret word is everything you need in life');
+    }
+    // logging the superarray to see whatsup
+    console.log('this is superArray after the SECRET WORD: ', superArray);
+
+    // validation check - if all booleans return false aka user says no/cancel to every option do this:
+    if ((lowercaseMsg === false) && (uppercaseMsg === false) && (numberMsg === false) && (specialCharMsg === false) && (secretMsg === false)) {
+        alert('you gotta choose something ya ding dong! let\'s try this again...')
+        Window.close();
+    }
     // bring it home with an empty password, this is gonna store the users actual random password once it's generated by our loop;
     var password = '';
 
+    // loop over the superArray and randomly grab indexes and push into the string password up to the user's specified length
     for (var index = 0; index < lengthMsg; index++) {
         var randomNumber = Math.floor(Math.random() * superArray.length); // so it's 0 t 1 (aka 0% to 100%)
         password += superArray[randomNumber];
-        console.log('this is the superArray in the loop', superArray)
+        // console.log('this is the superArray in the loop', superArray)
     };
 
     return password;
